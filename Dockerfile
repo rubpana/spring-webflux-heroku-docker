7cf565ff94d9bcc7875373b456e9cd4221d53323
rpanadero <rpanadero@okode.com>
2018-03-25 22:02:13 +0200

    Fixing deployment on heroku

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,8 @@
-FROM rubpana/spring-boot-app
-COPY /target/*.jar /data/target/app.jar
+FROM java:8
+
+# Just used in testing (Heroku doesn't support this command)
+EXPOSE 8080
+
+COPY target/spring-webflux-example-0.0.1-SNAPSHOT.jar /app.jar
+
+CMD ["java", "-jar", "/app.jar"]
--- src/main/java/com/rubfreelancer/springwebflux/SpringWebfluxExampleApplication.java
+++ src/main/java/com/rubfreelancer/springwebflux/SpringWebfluxExampleApplication.java
@@ -2,6 +2,8 @@ package com.rubfreelancer.springwebflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringWebfluxExampleApplication {
@@ -9,4 +11,25 @@ public class SpringWebfluxExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebfluxExampleApplication.class, args);
 	}
+	
+	@Bean
+	public TomcatServletWebServerFactory tomcatCustomizer() {
+	  TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+	  factory.addConnectorCustomizers((connector -> {
+		  
+		  /**
+		   * Gets PORT env variable and sets it as tomcat server port.
+		   * In Heroku env, the PORT env variable must be used by tomcat
+		   * because if not the process will fail.
+		   * Error R10 (Boot timeout) -> Web process failed to bind to $PORT within 60 seconds of launch
+		   */
+		  String envPort = System.getenv("PORT");
+		  // If PORT is NULL, we'll use the default port
+		  if (envPort != null) {
+		    connector.setPort(Integer.parseInt(envPort));
+		  }
+	  }));
+	  return factory;
+	}
+	
 }
